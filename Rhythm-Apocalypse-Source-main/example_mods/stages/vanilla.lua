c3dcca3f008ae475614a6c3b95ee03196cccb3a8
jac0bb <jacobsb440@outlook.com>
2023-01-13 20:58:06 -0500

    GOOFY GAMES

--- Rhythm-Apocalypse-Source-main/example_mods/stages/vanilla.lua
+++ Rhythm-Apocalypse-Source-main/example_mods/stages/vanilla.lua
@@ -4,5 +4,18 @@ function onCreate()
 	scaleObject('vanBG', 2.7, 2.7);
 	addLuaSprite('vanBG')
 	addGlitchEffect('vanBG', 2, 2)
+	setProperty('vanBG.antialiasing', false)
 
+end
+
+local totalTime = 0.0
+function onUpdatePost(elapsed)
+    totalTime = totalTime + elapsed
+
+    setScrollFactor("gf", 0.6, 0.6)
+    setProperty("gf.scale.x",0.4)
+    setProperty("gf.scale.y",0.4)
+    setProperty("gf.y",250+math.sin(totalTime*math.pi/3)*200)
+    setProperty("gf.x",-1000+(totalTime*400%3500))
+    setProperty("gf.angle",totalTime*40)
 end
