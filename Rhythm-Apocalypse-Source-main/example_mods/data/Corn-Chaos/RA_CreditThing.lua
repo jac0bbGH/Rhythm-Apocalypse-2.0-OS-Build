957a848361bf54aa3fb86a7ecaf1246f6a3a6ce8
jac0bb <jacobsb440@outlook.com>
2023-01-21 21:17:15 -0500

    cornchaos credits

--- Rhythm-Apocalypse-Source-main/example_mods/data/Corn-Chaos/RA_CreditThing.lua
+++ Rhythm-Apocalypse-Source-main/example_mods/data/Corn-Chaos/RA_CreditThing.lua
@@ -8,7 +8,7 @@
 
 local hexcode = "148c1e" -- to anyone reading, I would've gotten this from dad.healthColorArray with an rgb to hex script but the color and alpha completely break, so you gotta do it manually.
 local composer = "MrDragadon"
-local charter = "Sider"
+local charter = "Ninten"
 local bonusnote = "This song was originally gonna be Oh-Shit v5"
 local bonusnoteon = true
 
